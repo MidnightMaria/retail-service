d9f3402e609c30c24584a98f5a18cdfba6c34ce5
MidnightMaria <agnesmarmarie@gmail.com>
2025-10-05 15:57:00 +0700

    Initial commit - retail service project setup

---
+++ retail-service/src/test/java/com/agnesmaria/retail_service/RetailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agnesmaria.retail_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
